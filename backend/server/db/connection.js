ba9f382604ff3763128b02af84c0ebee464d411d
thanhphuongne <lephuong14052004@gmail.com>
2025-11-26 20:53:02 +0700

    connect to postgre

--- backend/server/db/connection.js
+++ backend/server/db/connection.js
@@ -12,18 +12,22 @@ import {
 
 // Sequelize Connection
 const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-// const sequelize = new Sequelize({
   host: DB_HOST,
   port: DB_PORT,
-  dialect: 'postgres', // 'postgres',
-  // storage: DB_PART,
-  // Các tùy chọn khác nếu cần
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Supabase requires SSL
+    }
+  },
   pool: {
     max: 10, // maximum number of connection in pool
     min: 0, // minimum number of connection in pool
     acquire: 30000, // maximum time, in milliseconds, that pool will try to get connection before throwing error
     idle: 10000 // maximum time, in milliseconds, that a connection can be idle before being released
-  }
+  },
+  logging: console.log // Enable logging for debugging
 });
 
 // Kiểm tra kết nối

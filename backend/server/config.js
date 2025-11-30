60a56f093e287fbc487e317ff6f029f30e6d4c33
thanhphuongne <lephuong14052004@gmail.com>
2025-11-30 23:27:33 +0700

    config

--- backend/server/config.js
+++ backend/server/config.js
@@ -65,3 +65,4 @@ export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 export const FROMEMAIL = process.env.FROMEMAIL;
 
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
